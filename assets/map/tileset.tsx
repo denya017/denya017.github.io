3e9b0a75f972e1a0464b0b224f7db11c988556eb
denya017 <78693014+denya017@users.noreply.github.com>
2021-02-17 18:04:18 +0200

    Create tileset.tsx

---
+++ assets/map/tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="tileset" tilewidth="32" tileheight="32" spacing="2" tilecount="90" columns="10">
+ <image source="tile0.png" width="338" height="312"/>
+</tileset>
